2716b3475aa1f43d3231a2c62c029b1b55fcb731
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-05-10 23:26:49 +0300

    fix: fake bug for travis checking

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
 it('should contain the text "Hello, world"', async () => {
   await page.goto('http://localhost:3000');
-  await expect(page).toMatch('H!!!ello, world')
+  await expect(page).toMatch('Hello, world')
 });
